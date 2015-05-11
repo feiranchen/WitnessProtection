1e8981c22ff36c135f77461fb402a8f5522d0e42
feiranchen <fc254@cornell.edu>
2015-05-10 21:14:03 -0400

    files added

---
+++ Rsample.m
@@ -0,0 +1,41 @@
+function f=Rsample(e, p, q)
+l= length(e);
+f=zeros(l*p/q,1);
+size(f)
+ii=1;
+for count = (1:l/q-1)
+        
+    for jj = (1:p)
+        f(count*p + jj)=e(ii)+(e(ii+q)-e(ii))*(jj-1)/p;
+    end
+    ii=ii+q;
+end
+
+
+
+
+
+
+
+
+% if r<=1
+%     count =1;
+%     for ii = (1:l)
+%         if count == 1/r
+%             f(ii*r)=e(ii);
+%             count =1;
+%         else
+%             count = count+1;
+%         end
+%     end
+% else
+%     for ii = (1:l)
+%         if ii==l
+%             f((ii-1)*r + jj)=e(ii);
+%         else
+%             for jj = (1:r)
+%                 f((ii-1)*r + jj)=e(ii)+(e(ii+1)-e(ii))*(jj-1)/r;
+%             end
+%         end
+%     end
+% end
---
+++ script.m
@@ -0,0 +1,10 @@
+[d,sr]=wavread('long.wav');
+e = pvoc(d, .75);
+
+
+
+soundsc(e,sr)
+f = Rsample(e,3,4); % NB: 0.8 = 4/5
+soundsc(f,sr) 
+f = resample(e,3,4); % NB: 0.8 = 4/5
+soundsc(f,sr)
